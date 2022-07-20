e4804c8742a3a565dbcd465714b070366e2b4f4a
GitHub Action <action@github.com>
2022-07-20 13:52:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/vinisauter/template/MyBundle.kt
+++ src/main/kotlin/com/github/vinisauter/template/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vinisauter.template
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/vinisauter/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vinisauter/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vinisauter.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vinisauter.template.MyBundle
 
 class MyApplicationService {
 
